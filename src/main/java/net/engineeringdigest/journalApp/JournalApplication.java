40e61156c0ed0507504e1b9b2af68bddba49fa46
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2025-09-15 19:27:22 +0600

    Created User entity

--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -3,7 +3,6 @@ package net.engineeringdigest.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class JournalApplication {
