6b15d0ed836b3778dca522897fd03e97588679c9
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2025-10-06 18:13:01 +0600

    mockito

--- src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
@@ -3,6 +3,7 @@ package net.engineeringdigest.journalApp.service;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled
 @SpringBootTest
 class UserServiceTests {
 
