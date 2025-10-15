1561aedf39eb9d0c7d8d9c5ba13177b6ee3c8430
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2025-10-15 17:03:04 +0600

    Adding Swagger

---
+++ src/main/java/net/engineeringdigest/journalApp/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package net.engineeringdigest.journalApp.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI myCustomConfig() {
+        return new OpenAPI().info(
+                new Info().title("Journal App APIs")
+                        .description("By Jack")
+        );
+    }
+
+}
--- src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
@@ -1,5 +1,6 @@
 package net.engineeringdigest.journalApp.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import net.engineeringdigest.journalApp.service.UserService;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User APIs", description = "Read, Update & Delete")
 public class UserController {
 
     @Autowired
